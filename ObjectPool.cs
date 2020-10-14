6426f4c0b2334b290773741b0516b97117c464bb
smas <sahab.masroor@gmail.com>
2020-10-14 19:14:55 +0330

    first commit

---
+++ ObjectPool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public List<GameObject> list;
+
+    private GameObject tObject;
+    private GameObject _prefab;
+
+    public ObjectPool(GameObject prefab, int depth)
+    {
+        _prefab = prefab;
+        for (int i = 0; i < depth; i++)
+        {
+            tObject = Instantiate(prefab);
+            tObject.SetActive(false);
+            list.Add(tObject);
+        }
+    }
+
+    private GameObject returnObj;
+    public GameObject GetObject()
+    {
+        if (list.Count <= 0)
+        {
+            GameObject g = Instantiate(_prefab);
+            g.SetActive(false);
+            list.Add(g);
+        }
+
+        foreach (var item in list)
+        {
+            if (!item.activeInHierarchy)
+            {
+                returnObj = item;
+                returnObj.SetActive(true);
+            }
+        }
+        return returnObj;
+    }
+     
+}
